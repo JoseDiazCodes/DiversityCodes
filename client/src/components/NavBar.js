0d18cd98a70d6c301d8d5e413361422dc4670731
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-10 17:49:07 -0500

    added hacks logo to the NavBar
    modified NavBar.js and App.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom"; // need to install : npm install react-router-dom
 
+
 const Navbar = () => {
   return (
     <nav className="navbar">
+    <div className="logo-container">
+    <img src="/favicon.png" alt="Logo" className="logo" />
+    </div>
       <ul className="navbar-nav">
         <li className="nav-item">
           <Link to="/" className="nav-link">
