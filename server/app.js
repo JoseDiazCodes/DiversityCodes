d8c04cafd37dd55be09439fb9385b3ed3b9c97cd
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-20 15:57:04 -0500

    commented out incomplete routes
    were causing compilation errors

--- server/app.js
+++ server/app.js
@@ -5,20 +5,20 @@ import cookieParser from "cookie-parser"
 import logger from "morgan"
 import cors from "cors"
 
-import indexRouter from "./routes/index"
-import usersRouter from "./routes/users"
+//import indexRouter from "./routes/index"
+//import usersRouter from "./routes/users"
 
-const app = express()
+const app = express();
 
 app.use(logger("dev"))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+//app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter)
-app.use("/users", usersRouter)
+//app.use("/", indexRouter)
+//app.use("/users", usersRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
