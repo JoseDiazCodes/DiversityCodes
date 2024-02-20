7e106ad4b6538d7831e7a475a683e7a50b31db4e
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-20 15:51:30 -0500

    added exam routes

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -1,3 +1,36 @@
-import { getAllExams } from "../controllers/exam.js"
+//import { getAllExams } from "../controllers/exam.js"
+//router.get("/exams", getAllExams)
 
-router.get("/exams", getAllExams)
+const express = require('express')
+
+const router = express.Router()
+
+// GET all exams 
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all exams'})
+})
+
+// GET single exam
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single exam'})
+})
+
+// POST a new exam
+router.post("/", (req, res) => {
+    //req.body
+    res.json({mssg: 'POST a new exam'})
+})
+
+// DELETE a exam
+router.delete("/:id", (req, res) => {
+    res.json({mssg: 'DELETE a exam'})
+})
+
+// UPDATE a exam
+router.patch("/:id", (req, res) => {
+    res.json({mssg: ' UPDATE a exam'})
+})
+
+module.exports = router
+
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,8 @@
-import express from "express"
-const router = express.Router()
+//import express from "express"
+//const router = express.Router()
 
-import UserController from "../controllers/user-controller"
+//import UserController from "../controllers/user-controller"
 
-router.get("/", UserController.getUser)
+//router.get("/", UserController.getUser)
 
-export default router
+//export default router
